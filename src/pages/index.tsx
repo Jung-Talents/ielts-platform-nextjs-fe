66655313205b0f886d32ee89c71fad22d71e8bbb
lexuan2602 <vongoclexuan@gmail.com>
2026-02-04 15:10:37 +0700

    Remove Test Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,6 @@ import { CardCTA, CardBenefits } from "../components";
 export default function Home() {
   return (
     <main className="bg-white">
-      <div className="w-full bg-blue-500 py-16 text-white text-center text-2xl">
-        Test Component
-      </div>
       <CardBenefits />
       <section className="mx-auto w-full max-w-[1440px] px-4 py-6">
         <div className="rounded-3xl bg-white shadow-sm ring-1 ring-zinc-200 overflow-hidden">
