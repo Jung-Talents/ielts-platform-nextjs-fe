777a40468114dd741a9206ab6f61857b898cb9a3
lexuan2602 <vongoclexuan@gmail.com>
2026-02-04 14:05:45 +0700

    Test/Test Build Fail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { CardCTA, CardBenefits } from "../components";
 export default function Home() {
   return (
     <main className="bg-white">
+      <Component_Not_Exist />
       <CardBenefits />
       <section className="mx-auto w-full max-w-[1440px] px-4 py-6">
         <div className="rounded-3xl bg-white shadow-sm ring-1 ring-zinc-200 overflow-hidden">
